7a6c8fbdfb8438510c8f18018461f19634cae8df
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2020-11-06 18:11:30 +0530

    AES and DES

---
+++ src/main/java/com/cipher/ciphertechniques/AES/AES.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cipher.ciphertechniques.AES;
+
+/**
+ *
+ * @author dhinesh
+ */
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+public class AES {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text = "", key = "";
+        while (true) {
+            System.out.println("\n1. Enter Text\n2. Enter the Key\n3. Encrypt \n4. Decrypt\nany for exit ");
+            int op = in .nextInt();
+            if (op == 1)
+                text = in .next();
+            else if (op == 2)
+                key = in .next();
+            else if (op == 3) {
+                System.out.println("Encrypted Text : " +
+                    encrypt(text, key));
+            } else if (op == 4) {
+                System.out.println("Decrypted Text : " +
+                    decrypt(text, key));
+            } else
+                break;
+        } in .close();
+    }
+    public static byte[] keygen(String key) throws
+    UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] keybytes = key.getBytes("UTF-8");
+        MessageDigest sha = MessageDigest.getInstance("SHA-1");
+        byte[] hashedkey = sha.digest(keybytes);
+        return Arrays.copyOf(hashedkey, 16);
+    }
+    public static String encrypt(String text, String key) {
+        String res = "Invalid Key";
+        try {
+            byte[] key128 = keygen(key);
+            Cipher cipher =
+                Cipher.getInstance("AES/ECB/PKCS5Padding");
+            SecretKeySpec secretKey = new SecretKeySpec(key128,
+                "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            res =
+                Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes(
+                    "UTF-8")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    public static String decrypt(String text, String key) {
+        String res = "Invalid Key";
+        try {
+            byte[] key128 = keygen(key);
+            Cipher cipher =
+                Cipher.getInstance("AES/ECB/PKCS5Padding");
+            SecretKeySpec secretKey = new SecretKeySpec(key128,
+                "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            res = new
+            String(cipher.doFinal(Base64.getDecoder().decode(text)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/cipher/ciphertechniques/AES/Main.java
@@ -0,0 +1,79 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class Main {
+    public static boolean isempty(JTextField t1, JTextField t2) {
+        String s1 = t1.getText();
+        String s2 = t2.getText();
+        if (s1.equals("") || s1.equals("Enter a number") ||
+            s2.equals("") || s2.equals("Enter a number"))
+            return true;
+        return false;
+    }
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
+    }
+    public Main() {
+        JFrame jf = new JFrame("AES-128");
+        jf.setLocation(500, 500);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(600, 160);
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        jf.add(p);
+        final JTextField text = new JTextField("Text");
+        final JTextField key = new JTextField("Key");
+        final JTextField ans = new
+        JTextField("Encrypted/Decrypted Text");
+        ans.setEditable(false);
+        p.add(text);
+        p.add(key);
+        p.add(ans);
+        JPanel jp = new JPanel();
+        jp.setLayout(new FlowLayout());
+        JButton enc = new JButton("Encrypt");
+        JButton dec = new JButton("Decrypt");
+        JButton clear = new JButton("Clear");
+        clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                ans.setText("");
+                text.setText("");
+                key.setText("");
+            }
+        });
+        enc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                if (isempty(text, key))
+                    ans.setText("Both key and text required");
+                else {
+                    ans.setText(AES.encrypt(text.getText(),
+                        key.getText()));
+                }
+            }
+        });
+        dec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                if (isempty(text, key))
+                    ans.setText("Both key and text required");
+                else {
+                    ans.setText(AES.decrypt(text.getText(),
+                        key.getText()));
+                }
+            }
+        });
+        jp.add(enc);
+        jp.add(dec);
+        jp.add(clear);
+        p.add(jp);
+        jf.setVisible(true);
+    }
+}
