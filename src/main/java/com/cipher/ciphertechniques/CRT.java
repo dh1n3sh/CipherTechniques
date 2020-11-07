31c2b78c7a6a2a42abff1edaad92117b58e25b9a
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2020-11-07 08:29:49 +0530

    crt working

--- src/main/java/com/cipher/ciphertechniques/CRT.java
+++ src/main/java/com/cipher/ciphertechniques/CRT.java
@@ -1,52 +1,79 @@
 package com.cipher.ciphertechniques;
 
-// A Java program to demonstrate the working of Chinese remainder 
-// Theorem 
 import java.io.*;
 
-class GFG {
-
-    // k is size of num[] and rem[]. Returns the smallest 
-    // number x such that: 
-    // x % num[0] = rem[0], 
-    // x % num[1] = rem[1], 
-    // .................. 
-    // x % num[k-2] = rem[k-1] 
-    // Assumption: Numbers in num[] are pairwise coprime 
-    // (gcd for every pair is 1) 
-    static int findMinX(int num[], int rem[], int k) {
-        int x = 1; // Initialize result 
-
-        // As per the Chinese remainder theorem, 
-        // this loop will always break. 
-        while (true) {
-            // Check if remainder of x % num[j] is 
-            // rem[j] or not (for all j from 0 to k-1) 
-            int j;
-            for (j = 0; j < k; j++) {
-                if (x % num[j] != rem[j]) {
-                    break;
-                }
+class CRT { 
+	
+	// Returns modulo inverse of a 
+	// with respect to m using extended 
+	// Euclid Algorithm. Refer below post for details: 
+	// https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/ 
+        
+        static int gcdExtended(int a, int b, int[] g){
+            // Base Case
+            if (a == 0) 
+            {
+                g[0] = 0;
+                g[1] = 1;
+                        
+                return b;
             }
 
-            // If all remainders matched, we found x 
-            if (j == k) {
-                return x;
-            }
+            int gcd = gcdExtended(b % a, a, g);
 
-            // Else try next numner 
-            x++;
+            // Update x and y using results of recursive
+            // call
+            int x = g[1] - (b / a) * g[0];
+            int y = g[0];
+            
+            g[0] = x;
+            g[1] = y;
+            return gcd;
         }
+	static int inv(int a, int m) 
+	{ 
+            int[] g = new int[2];
+            int gcd = gcdExtended(a, m, g);
+            if (gcd != 1)
+                return -1;
+            else
+            {
+                // m is added to handle negative x
+                int res = (g[0] % m + m) % m;
+                return res;
+            }
+	} 
+	
+	// Assumption: Numbers in num[] are pairwise 
+	// coprime (gcd for every pair is 1) 
+	static int findMinX(int num[], int rem[], int k) 
+	{ 
+		// Compute product of all numbers 
+		int prod = 1; 
+		for (int i = 0; i < k; i++) 
+			prod *= num[i]; 
+	
+		// Initialize result 
+		int result = 0; 
+	
+		// Apply above formula 
+		for (int i = 0; i < k; i++) 
+		{ 
+			int pp = prod / num[i]; 
+			result += rem[i] * inv(pp, num[i]) * pp; 
+		} 
+	
+		return result % prod; 
+	} 
+	
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		int num[] = {3, 4, 5}; 
+		int rem[] = {2, 3, 1}; 
+		int k = num.length; 
+		System.out.println("x is " +findMinX(num, rem, k)); 
+	} 
+} 
 
-    }
-
-    // Driver method 
-    public static void main(String args[]) {
-        int num[] = {3, 4, 5};
-        int rem[] = {2, 3, 1};
-        int k = num.length;
-        System.out.println("x is " + findMinX(num, rem, k));
-    }
-}
-
-/*This code is contributed by Nikita Tiwari.*/
+// This code is contributed by nikita Tiwari. 
